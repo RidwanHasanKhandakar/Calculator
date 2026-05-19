371f2d7bb9d5c8cfe5e70075c0f3f65a182af343
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-19 15:12:59 +0600

    imported nes & coded the main logic

--- src/main/java/com/example/class_19th/ArithmeticOPController.java
+++ src/main/java/com/example/class_19th/ArithmeticOPController.java
@@ -2,7 +2,8 @@ package com.example.class_19th;
 
 import javafx.event.ActionEvent;
 
-import java.awt.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ArithmeticOPController {
 
@@ -21,22 +22,34 @@ public class ArithmeticOPController {
     int firstNum = Integer.parseInt(firstNumTextField.getText());
     int secondNum = Integer.parseInt(secondNumTextField.getText());
     int answer = firstNum+secondNum;
-    errorLabel.setText("firstNum + secondNum");
+    errorLabel.setText("firstNum + secondNum = " + answer);
     //resTextArea.setText("Answer: "+answer);
     }
 
     @javafx.fxml.FXML
     public void handleMinusButton(ActionEvent actionEvent) {
+        int firstNum = Integer.parseInt(firstNumTextField.getText());
+        int secondNum = Integer.parseInt(secondNumTextField.getText());
+        int answer = firstNum-secondNum;
+        errorLabel.setText("firstNum - secondNum = " + answer);
 
     }
 
     @javafx.fxml.FXML
     public void handleMultipleButton(ActionEvent actionEvent) {
+        int firstNum = Integer.parseInt(firstNumTextField.getText());
+        int secondNum = Integer.parseInt(secondNumTextField.getText());
+        int answer = firstNum*secondNum;
+        errorLabel.setText("firstNum * secondNum = " + answer);
 
     }
 
     @javafx.fxml.FXML
     public void handleDivideButton(ActionEvent actionEvent) {
+        int firstNum = Integer.parseInt(firstNumTextField.getText());
+        int secondNum = Integer.parseInt(secondNumTextField.getText());
+        int answer = firstNum/secondNum;
+        errorLabel.setText("firstNum / secondNum = " + answer);
 
     }
 }
