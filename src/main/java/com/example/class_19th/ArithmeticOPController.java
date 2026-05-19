d02f52da9f32b672c91c697a4fc80fb6ae57c4ff
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-19 14:14:03 +0600

    added all the nes fields & buttons also updated the controller class

--- src/main/java/com/example/class_19th/ArithmeticOPController.java
+++ src/main/java/com/example/class_19th/ArithmeticOPController.java
@@ -1,5 +1,18 @@
 package com.example.class_19th;
 
+import javafx.event.ActionEvent;
+
 public class ArithmeticOPController {
 
+    public void handlePlusButton(ActionEvent actionEvent) {
+    }
+
+    public void handleMinusButton(ActionEvent actionEvent) {
+    }
+
+    public void handleMultipleButton(ActionEvent actionEvent) {
+    }
+
+    public void handleDivideButton(ActionEvent actionEvent) {
+    }
 }
